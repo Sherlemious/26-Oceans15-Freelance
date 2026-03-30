106a2222579637253df0e0914a50c1796fba9e06
Abdelrahman Mohammed <abd.moh.yousef@gmail.com>
2026-03-30 22:46:55 +0200

    init: project setup by (Abdelrahman_Mohammed_55-4626)

---
+++ job-service/src/main/java/com/team26/freelance/job/JobApplication.java
@@ -0,0 +1,11 @@
+package com.team26.freelance.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JobApplication.class, args);
+    }
+}
---
+++ proposal-service/src/main/java/com/team26/freelance/proposal/ProposalApplication.java
@@ -0,0 +1,11 @@
+package com.team26.freelance.proposal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProposalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProposalApplication.class, args);
+    }
+}
