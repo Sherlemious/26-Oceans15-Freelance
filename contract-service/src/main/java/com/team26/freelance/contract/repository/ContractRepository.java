0a3b33baaefd4836e6194406c54ff937db9eb494
yousefyasser <yousef.mostafa@student.guc.edu.eg>
2026-04-06 22:29:07 +0200

    feat(contract-service): add entities and basic CRUD (55-3437)

---
+++ contract-service/src/main/java/com/team26/freelance/contract/repository/ContractRepository.java
@@ -0,0 +1,9 @@
+package com.team26.freelance.contract.repository;
+
+import com.team26.freelance.contract.model.Contract;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContractRepository extends JpaRepository<Contract, Long> {
+}
