2ba017c94c6256c5aede32428270ed0261da8cd0
Mariam Aslan <140263162+mariamM13@users.noreply.github.com>
2026-04-07 22:58:38 +0200

    feat(job-service): add CRUD for job (55-0837)

---
+++ job-service/src/main/java/com/team26/freelance/job/controller/JobController.java
@@ -0,0 +1,46 @@
+package com.team26.freelance.job.controller;
+
+import com.team26.freelance.job.model.Job;
+import com.team26.freelance.job.service.JobService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/jobs")
+public class JobController {
+
+    private final JobService jobService;
+
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @PostMapping
+    public Job createJob(@RequestBody Job job) {
+        return jobService.createJob(job);
+    }
+
+    @GetMapping
+    public List<Job> getAllJobs() {
+        return jobService.getAllJobs();
+    }
+
+    @GetMapping("/{id}")
+    public Job getJobById(@PathVariable Long id) {
+        return jobService.getJobById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Job updateJob(
+            @PathVariable Long id,
+            @RequestBody Job job
+    ) {
+        return jobService.updateJob(id, job);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteJob(@PathVariable Long id) {
+        jobService.deleteJob(id);
+    }
+}
