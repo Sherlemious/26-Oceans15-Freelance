acfb7a26bfa34046f3e65c7b5c9a4d35910da03d
Yassin Bareh <yassinbareh13@gmail.com>
2026-04-11 22:04:39 +0200

    feat(user-service): Add endpoint to update user preferences (55-3410)

--- user-service/src/main/java/com/team26/freelance/user/dto/UserResponseDTO.java
+++ user-service/src/main/java/com/team26/freelance/user/dto/UserResponseDTO.java
@@ -45,5 +45,4 @@ public class UserResponseDTO {
     public Map<String, Object> getPreferences() { return preferences; }
     public LocalDateTime getCreatedAt() { return createdAt; }
     public List<UserSkillResponseDTO> getUserSkills() { return userSkills; }
-    //s1-f2
 }
