69fd1b57d4909fe7bf2943a23c0679fff1257233
yousefyasser <yousef.mostafa@student.guc.edu.eg>
2026-04-11 03:46:17 +0200

    fix(proposal-service): add missing imports (55-3437)

--- proposal-service/src/main/java/com/team26/freelance/proposal/repository/ProposalRepository.java
+++ proposal-service/src/main/java/com/team26/freelance/proposal/repository/ProposalRepository.java
@@ -2,6 +2,7 @@ package com.team26.freelance.proposal.repository;
 
 import com.team26.freelance.proposal.model.Proposal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
