b141359499795bee3a1d938378ad308c5cbbcdff
SeifAbbas <saifomaster4@gmail.com>
2026-05-18 19:22:38 +0300

    build(m3): setup OpenFeign, actuator, and service URLs (55-25218)

--- proposal-service/src/main/java/com/team26/freelance/proposal/ProposalApplication.java
+++ proposal-service/src/main/java/com/team26/freelance/proposal/ProposalApplication.java
@@ -2,8 +2,10 @@ package com.team26.freelance.proposal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = "com.team26.freelance")
+@EnableFeignClients(basePackages = "com.team26.freelance.contracts.feign")
 public class ProposalApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProposalApplication.class, args);
