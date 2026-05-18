b768910046d2c6562b7b69e2bcfa92da299952aa
Mariam Aslan <140263162+mariamM13@users.noreply.github.com>
2026-05-18 22:59:14 +0300

    fix(job): remove redundant import (55-0837)

--- job-service/src/main/java/com/team26/freelance/job/config/JobEventConfig.java
+++ job-service/src/main/java/com/team26/freelance/job/config/JobEventConfig.java
@@ -5,7 +5,6 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Qualifier;
